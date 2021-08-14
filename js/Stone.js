7e267d83566d08e1cbbd4deec5472dffa07ba219
Neellesh <85873752+Neellesh@users.noreply.github.com>
2021-08-14 14:48:19 +0530

    Update Stone.js

--- js/Stone.js
+++ js/Stone.js
@@ -7,7 +7,7 @@
         this.r = 30;
         this.width = width;
         this.height = height;
-        this.body = Bodies.circle(x, y, this.r);
+        this.body = Bodies.circle(x, y, this.r, options);
         World.add(world, this.body); 
      }
 
@@ -17,7 +17,7 @@
 
         push();
         ellipseMode(RADIUS);
-        ellipse(pos.x,pos.y,this.r,options)
+        ellipse(pos.x,pos.y,this.r)
         pop();
      }
  }
